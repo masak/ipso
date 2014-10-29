b6151561d4fe6a067375c3f4196593e6da014e15
Carl Masak <cmasak@gmail.com>
2014-10-29 23:51:29 +0100

    implement eval
    And two helper routines that it uses.

--- lib/Ipso.pm
+++ lib/Ipso.pm
@@ -63,6 +63,41 @@ my %SETTING =
         (lambda (x y)
           (cond ((eq (caar y) x) (cadar y))
                 ('t (assoc x (cdr y)))))]),
+    eval => parse(q[
+        (lambda (e a)
+          (cond
+            ((atom e) (assoc e a))
+            ((atom (car e))
+             (cond
+               ((eq (car e) 'quote) (cadr e))
+               ((eq (car e) 'atom)  (atom     (eval (cadr e) a)))
+               ((eq (car e) 'eq)    (eq       (eval (cadr e) a)
+                                              (eval (caddr e) a)))
+               ((eq (car e) 'car)   (car      (eval (cadr e) a)))
+               ((eq (car e) 'cdr)   (cdr      (eval (cadr e) a)))
+               ((eq (car e) 'cons)  (cons     (eval (cadr e) a)
+                                              (eval (caddr e) a)))
+               ((eq (car e) 'cond)  (evcon    (cdr e) a))
+               ('t (eval (cons (assoc (car e) a)
+                               (cdr e))
+                         a))))
+            ((eq (caar e) 'label)
+             (eval (cons (caddar e) (cdr e))
+                   (cons (cons (cadar e) (cons (car e) '())) a)))
+            ((eq (caar e) 'lambda)
+             (eval (caddar e)
+                   (append (pair (cadar e) (evlis (cdr e) a))
+                           a)))))]),
+    evcon => parse(q[
+        (lambda (c a)
+          (cond ((eval (caar c) a)
+                 (eval (cadar c) a))
+                ('t (evcon (cdr c) a))))]),
+    evlis => parse(q[
+        (lambda (m a)
+          (cond ((null m) '())
+                ('t (cons (eval  (car m) a)
+                          (evlis (cdr m) a)))))]),
 ;
 
 class X::NoSuchSymbol is Exception {
